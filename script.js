12d4b6e47e17f8886679d9e2a5598cd47c00730a
abhishek singh <abhishekkumarsingh276642@gmail.com>
2024-07-08 13:58:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//let score='33abc';
+//let score=null;
+let score=undefined  ;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber =Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+let someNumber = 30;
+let strinNumber= String(someNumber)
+console.log(strinNumber)
+console.log( typeof strinNumber)
